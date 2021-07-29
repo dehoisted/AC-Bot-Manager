2e984e1bcef60dd99354cb4e6c0074469df4ad7f
orlando <75084509+dehoisted@users.noreply.github.com>
2021-07-28 20:23:02 -0400

    Delete Bot-Manager directory

--- Bot-Manager/pch.cpp
+++
@@ -1,3 +0,0 @@
-// pch.cpp: source file corresponding to the pre-compiled header (needed)
-
-#include "pch.h"
--- Bot-Manager/pch.h
+++
@@ -1,8 +0,0 @@
-// pch.h: This is a precompiled header file.
-
-#ifndef PCH_H
-#define PCH_H
-
-#include "framework.h"
-
-#endif //PCH_H
